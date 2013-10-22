78988928741b613dc89c3de3c1365b93b866c034
Dom Guinard <dev@guinard.org>
2013-10-22 11:15:05 +0200

    Initial commit

---
+++ MyHelloADK/gen/org/webofthings/adk/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package org.webofthings.adk;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
